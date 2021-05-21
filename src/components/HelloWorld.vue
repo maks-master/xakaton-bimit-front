51b9001e6f455df2a716c46e914e9891df6ee2e0
nomatter <nomatter@mifors.com>
2021-05-21 23:14:33 +0400

    add some free mesh to loaded model, its works

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,8 @@
   import { Viewer } from '@xeokit/xeokit-sdk/src/viewer/Viewer.js'
   import { XKTLoaderPlugin } from '@xeokit/xeokit-sdk/src/plugins/XKTLoaderPlugin/XKTLoaderPlugin.js'
 
+  import {Mesh, Node, PhongMaterial, buildBoxGeometry, ReadableGeometry} from "@xeokit/xeokit-sdk/src/viewer/scene"
+
   export default {
     name: 'HelloWorld',
 
@@ -27,19 +29,97 @@
 
         this.xktLoader = new XKTLoaderPlugin(this.viewer);
 
-        const model = this.xktLoader.load({
+        this.model = this.xktLoader.load({
           id: "myModel",
           src: "./model/ov.xkt",
           metaModelSrc: "./model/ov.json",
           edges: true
         });
 
-        model.on("loaded", () => {
+        this.model.on("loaded", () => {
           console.log('model loaded')
-          this.viewer.cameraFlight.flyTo(model)
+          this.addMesh()
+          this.viewer.cameraFlight.flyTo(this.model)
         });
+      },
 
+      addMesh(){
+        const boxGeometry = new ReadableGeometry(this.viewer.scene, buildBoxGeometry({
+            xSize: 1,
+            ySize: 1,
+            zSize: 1
+        }));
 
+        new Node(this.viewer.scene, {
+            id: "table",
+            isModel: false, // <---------- Node represents a model, so is registered by ID in viewer.scene.models
+            rotation: [0, 50, 0],
+            position: [0, 0, 0],
+            scale: [1, 1, 1],
+
+            children: [
+
+                new Mesh(this.viewer.scene, { // Red table leg
+                    id: "redLeg",
+                    isObject: true, // <------ Node represents an object, so is registered by ID in viewer.scene.objects
+                    position: [-4, -6, -4],
+                    scale: [1, 3, 1],
+                    rotation: [0, 0, 0],
+                    material: new PhongMaterial(this.viewer.scene, {
+                        diffuse: [1, 0.3, 0.3]
+                    }),
+                    geometry: boxGeometry
+                }),
+
+                new Mesh(this.viewer.scene, { // Green table leg
+                    id: "greenLeg",
+                    isObject: true, // <------ Node represents an object, so is registered by ID in viewer.scene.objects
+                    position: [4, -6, -4],
+                    scale: [1, 3, 1],
+                    rotation: [0, 0, 0],
+                    material: new PhongMaterial(this.viewer.scene, {
+                        diffuse: [0.3, 1.0, 0.3]
+                    }),
+                    geometry: boxGeometry
+                }),
+
+                new Mesh(this.viewer.scene, {// Blue table leg
+                    id: "blueLeg",
+                    isObject: true, // <------ Node represents an object, so is registered by ID in viewer.scene.objects
+                    position: [4, -6, 4],
+                    scale: [1, 3, 1],
+                    rotation: [0, 0, 0],
+                    material: new PhongMaterial(this.viewer.scene, {
+                        diffuse: [0.3, 0.3, 1.0]
+                    }),
+                    geometry: boxGeometry
+                }),
+
+                new Mesh(this.viewer.scene, {  // Yellow table leg
+                    id: "yellowLeg",
+                    isObject: true, // <------ Node represents an object, so is registered by ID in viewer.scene.objects
+                    position: [-4, -6, 4],
+                    scale: [1, 3, 1],
+                    rotation: [0, 0, 0],
+                    material: new PhongMaterial(this.viewer.scene, {
+                          diffuse: [1.0, 1.0, 0.0]
+                    }),
+                    geometry: boxGeometry
+                }),
+
+                new Mesh(this.viewer.scene, { // Purple table top
+                    id: "tableTop",
+                    isObject: true, // <------ Node represents an object, so is registered by ID in viewer.scene.objects
+                    position: [0, -3, 0],
+                    scale: [6, 0.5, 6],
+                    rotation: [0, 0, 0],
+                    material: new PhongMaterial(this.viewer.scene, {
+                        diffuse: [1.0, 0.3, 1.0]
+                    }),
+                    geometry: boxGeometry
+                })
+            ]
+        });
       }
     }
   }
