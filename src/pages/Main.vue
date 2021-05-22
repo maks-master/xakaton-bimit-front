ca65b10c4845c4eaba59d84ef0703f783aa84a22
Ivan Isaev <blazer@mifors.com>
2021-05-22 09:43:40 +0400

    [Viewer] показ этажей

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -2,8 +2,7 @@
 
   div
     canvas#myCanvas
-
-    
+    div#storeys
 
 </template>
 
@@ -12,6 +11,7 @@
   import { XKTLoaderPlugin } from '@xeokit/xeokit-sdk/src/plugins/XKTLoaderPlugin/XKTLoaderPlugin.js'
 
   import {Mesh, Node, PhongMaterial, buildBoxGeometry, ReadableGeometry} from "@xeokit/xeokit-sdk/src/viewer/scene"
+  import { StoreyViewsPlugin, math, CameraMemento } from "@xeokit/xeokit-sdk"
 
   export default {
     data: () => ({
@@ -37,13 +37,17 @@
           src: "./model/ov.xkt",
           metaModelSrc: "./model/ov.json",
           edges: true
-        });
+        })
+
+        this.storeyViewsPlugin = new StoreyViewsPlugin(this.viewer)
 
         this.model.on("loaded", () => {
           console.log('model loaded')
-          this.addMesh()
+          // this.addMesh()
           this.viewer.cameraFlight.flyTo(this.model)
-        });
+          this.viewer.scene.setObjectsOpacity(this.viewer.metaScene.getObjectIDsByType("IfcDoor"), 0.3)
+          this.buildStoreyMapsMenu()
+        })
 
         this.viewer.cameraControl.on("picked", (pickResult) => {
           console.log('‼️ Res pickResult:', pickResult)
@@ -51,10 +55,108 @@
           console.log('‼️ Res pickResult.position:', pickResult.position)
           console.log('‼️ Res pickResult.entity.id:', pickResult.entity.id)
         })
-
       },
 
-      addMesh(){
+      buildStoreyMapsMenu() {
+        const cameraMemento = new CameraMemento() // Saves 3D perspective camera to restore
+        cameraMemento.saveCamera(this.viewer.scene)
+
+        const storeyDiv = document.getElementById("storeys")
+        const storeyIds = Object.keys(this.storeyViewsPlugin.storeys);
+
+        const canStandOnTypes = { // IFC types we can stand on in first-person mode
+            IfcSlab: true,
+            IfcStair: true,
+            IfcFloor: true,
+            IfcFooting: true
+        };
+
+        for (var i = 0, len = storeyIds.length; i < len; i++) {
+
+            const storeyId = storeyIds[i];
+
+            const storeyMap = this.storeyViewsPlugin.createStoreyMap(storeyId, {
+                format: "png",
+                width: 300,
+                useObjectStates: true
+            });
+
+            const img = document.createElement("img");
+            img.src = storeyMap.imageData;
+            img.style.border = "1px solid #000000";
+            img.style.background = "lightblue";
+            img.style.width = storeyMap.width + "px";
+            img.style.height = storeyMap.height + "px";
+            img.style.opacity = 0.8;
+
+            storeyDiv.appendChild(img);
+
+            img.onmouseenter = () => {
+                img.style.cursor = "default";
+            };
+
+            img.onmousemove = (e) => {
+                img.style.cursor = "default";
+                const imagePos = [e.offsetX, e.offsetY];
+                const pickResult = this.storeyViewsPlugin.pickStoreyMap(storeyMap, imagePos, {});
+                if (pickResult) {
+                    const entity = pickResult.entity;
+                    const metaObject = this.viewer.metaScene.metaObjects[entity.id];
+                    if (metaObject) {
+                        if (canStandOnTypes[metaObject.type]) {
+                            img.style.cursor = "pointer";
+                        }
+                    }
+                }
+            };
+
+            img.onmouseleave = () => {
+                img.style.cursor = "default";
+            };
+
+            const worldPos = math.vec3();
+
+            img.onclick = (e) => {
+
+                const imagePos = [e.offsetX, e.offsetY];
+
+                const pickResult = this.storeyViewsPlugin.pickStoreyMap(storeyMap, imagePos, {
+                    pickSurface: true
+                });
+
+                if (pickResult) {
+
+                    worldPos.set(pickResult.worldPos);
+
+                    // Set camera vertical position at the mid point of the storey's vertical
+                    // extents - note how this is adapts to whichever of the X, Y or Z axis is
+                    // designated the World's "up" axis
+
+                    const camera = this.viewer.scene.camera;
+                    const idx = camera.xUp ? 0 : (camera.yUp ? 1 : 2); // Find the right axis for "up"
+                    const storey = this.storeyViewsPlugin.storeys[storeyMap.storeyId];
+                    worldPos[idx] = (storey.aabb[idx] + storey.aabb[3 + idx]) / 2;
+
+                    this.viewer.cameraFlight.flyTo({
+                        eye: worldPos,
+                        up: this.viewer.camera.worldUp,
+                        look: math.addVec3(worldPos, this.viewer.camera.worldForward, []),
+                        projection: "perspective",
+                        duration: 1.5
+                    }, () => {
+                        this.viewer.cameraControl.navMode = "firstPerson";
+                        this.viewer.cameraControl.followPointer = false;
+                    });
+                } else {
+                    cameraMemento.restoreCamera(this.viewer.scene, () => {
+                        this.viewer.cameraControl.navMode = "planView";
+                    });
+                }
+            };
+        }
+      },
+
+      addMesh () {
         const boxGeometry = new ReadableGeometry(this.viewer.scene, buildBoxGeometry({
             xSize: 1,
             ySize: 1,
@@ -130,12 +232,15 @@
                     geometry: boxGeometry
                 })
             ]
-        });
+        })
       }
     }
   }
 </script>
 
 <style scoped>
-  #myCanvas {width:100%;height:100%;}
+#myCanvas {
+  width: 100%;
+  height: 400px;
+}
 </style>
