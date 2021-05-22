b781bb3513b88bca74e0629541b5e57a6032278e
Alex <nomatter@mifors.com>
2021-05-22 09:13:43 +0400

    layout, page

---
+++ src/layouts/Default.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  v-app(v-resize="onResize")
+    v-app-bar(app elevation="1" )
+
+    v-main.mb-3(app)
+      v-container.mw-1280
+        router-view
+
+    v-footer.py-5(padless)
+     
+
+</template>
+
+<script>
+  
+
+  export default {
+    components:{
+      
+    },
+
+    data: () => ({
+      
+    }),
+
+    mounted () {
+      
+    },
+
+    computed: {
+      
+    },
+
+    watch:{
+      
+    },
+
+    methods: {
+      
+    }
+  }
+</script>
