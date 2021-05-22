1bc0d45e008ea2ed79e6c2d21464093bd62f3b3b
Alex <nomatter@mifors.com>
2021-05-22 16:38:43 +0400

    1

--- src/components/DeviceList.vue
+++ src/components/DeviceList.vue
@@ -4,8 +4,25 @@
       v-list-item-content
         v-list-item-title Датчики
 
+    v-divider
+    v-list-item
+      v-list-item-content
+        v-list-item-title Привязанные
+    v-divider
+
     v-list(dense)
-      v-list-item(v-for="d in devices" :key="d.uuid" :device="d")
+      v-list-item(v-for="d in assignedDevices" :key="d.uuid")
+        v-list-item-content
+          v-list-item-title {{ d.name }}
+
+    v-divider.mb-2
+    v-list-item
+      v-list-item-content
+        v-list-item-title Не привязанные
+    v-divider
+
+    v-list(dense)
+      v-list-item(v-for="d in notAssignedDevices" :key="d.uuid")
         v-list-item-content
           v-list-item-title {{ d.name }}
       
@@ -30,6 +47,12 @@ export default {
 
   computed: {
     ...mapGetters(['devices']),
+    assignedDevices(){
+      return this.devices.filter(d => d.elementId && d.position)
+    },
+    notAssignedDevices(){
+      return this.devices.filter(d => !d.elementId || !d.position)
+    }
   },
 }
 </script>
